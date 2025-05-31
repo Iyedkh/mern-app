2a9006f57c0ff69e11ecaa18a2262e2cc2a60120
iyedkh <111179698+Iyedkh@users.noreply.github.com>
2025-05-31 18:39:03 +0100

    third commit
    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
-
 function App() {
+  const [names, setNames] = useState([]);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    axios.get('/names').then(res => setNames(res.data));
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post('http://localhost:5000/names', { value: input });
+    setNames([...names, res.data]);
+    setInput('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '2rem' }}>
+      <h1>Simple MERN App</h1>
+      <form onSubmit={handleSubmit}>
+        <input value={input} onChange={e => setInput(e.target.value)} />
+        <button type="submit">Add Name</button>
+      </form>
+      <ul>
+        {names.map((n, idx) => <li key={idx}>{n.value}</li>)}
+      </ul>
     </div>
   );
 }
